0b4777dd45c3f9f740be12eb3ec9dd0a9300b2de
jaehyun1ee <jaehyun.lee@furiosa.ai>
2023-01-06 16:40:10 +0900

    Added center node to context

--- src/command.rs
+++ src/command.rs
@@ -4,6 +4,19 @@ use crate::error::ViewerError;
 use crate::graph::{ Node, EdgeMap };
 use repl_rs::{ Value, Convert };
 
+pub fn show(_args: HashMap<String, Value>, context: &mut Context) -> Result<Option<String>, ViewerError> {
+    let graph = &context.graph;
+    let center = &context.center;
+    let depth = &context.depth;
+
+    let edgemap = EdgeMap::new(graph);
+    Ok(Some(format!(
+        "{}\n{}",
+        edgemap.render(center, *depth),
+        context.to_string()
+    )))
+}
+
 pub fn goto(args: HashMap<String, Value>, context: &mut Context) -> Result<Option<String>, ViewerError> {
     let graph = &context.graph;
     let depth = &context.depth;
--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,13 @@
-use crate::graph::Graph;
+use crate::graph::{ Graph, Node };
 
-pub struct Context {
-    pub graph: Graph,
+pub struct Context<'a> {
+    pub graph: &'a Graph,
+    pub center: &'a Node,
     pub depth: u8,
+}
+
+impl<'a> Context<'a> {
+    pub fn to_string(&self) -> String {
+        format!("center : {}\ndepth: {}\n", &self.center.id, self.depth)
+    }
 }
