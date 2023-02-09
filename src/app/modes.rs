406e6923e148c2d2292e404daf1722535aaeac26
jaehyun1ee <jaehyun.lee@furiosa.ai>
2023-02-09 14:28:59 +0900

    doc: mode

--- src/app/modes.rs
+++ src/app/modes.rs
@@ -1,16 +1,24 @@
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+/// `Mode` represents the context that the application, `dot-viewer` is in.
 pub enum Mode {
     Main(MainMode),
     Popup,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+/// In `MainMode`, users can
+/// - navigate the current, prev, next lists, or
+/// - type in inputs to the input form.
 pub enum MainMode {
     Navigate(NavMode),
     Input(InputMode),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+/// In `NavMode`, users can navigate
+/// - current nodes list,
+/// - previous nodes list, and
+/// - next nodes list.
 pub enum NavMode {
     Current,
     Prevs,
@@ -18,12 +26,18 @@ pub enum NavMode {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+/// In `InputMode`, users can type in inputs for,
+/// - node search, or
+/// - filter application with node id prefix
 pub enum InputMode {
     Search(SearchMode),
     Filter,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+/// In `SearchMode`, users can search for a node with,
+/// - fuzzy search against node ids, or
+/// - regex search against raw node representation in dot format.
 pub enum SearchMode {
     Fuzzy,
     Regex,
