75e6729f87c811e605806cf55b2f2e59c57e3115
jaehyun1ee <jaehyun.lee@furiosa.ai>
2023-02-14 16:11:48 +0900

    feat: simplify help message in input form

--- src/ui/input.rs
+++ src/ui/input.rs
@@ -38,7 +38,7 @@ pub(super) fn draw_input<B: Backend>(
         .margin(1)
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
         .split(chunk);
-    draw_help(f, chunks[0], mmode, app);
+    draw_help(f, chunks[0]);
     match mmode {
         MainMode::Navigate(_) => draw_error(f, chunks[1], app),
         MainMode::Input(_) => draw_form(f, chunks[1], mmode, app),
@@ -46,68 +46,17 @@ pub(super) fn draw_input<B: Backend>(
 }
 
 // help block
-fn draw_help<B: Backend>(f: &mut Frame<B>, chunk: Rect, mmode: &MainMode, _app: &mut App) {
-    let (msg, style) = match &mmode {
-        MainMode::Navigate(_) => (
-            vec![
-                Span::raw("Press "),
-                Span::styled("q", Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)),
-                Span::raw(" to exit, "),
-                Span::styled("/", Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)),
-                Span::raw(" to fuzzy search, "),
-                Span::styled("f", Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)),
-                Span::raw(" to apply filter with prefix, "),
-                Span::styled("e", Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)),
-                Span::raw(" to export the current subgraph, "),
-                Span::styled("0-9", Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)),
-                Span::raw(" to export the current neighbors with specified degree, "),
-                Span::styled("c", Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)),
-                Span::raw(" to close tab, and "),
-                Span::styled(
-                    "tab/backtab",
-                    Style::default().fg(Color::Red).add_modifier(Modifier::BOLD),
-                ),
-                Span::raw(" to change tab."),
-            ],
-            Style::default().add_modifier(Modifier::RAPID_BLINK),
-        ),
-        MainMode::Input(imode) => match imode {
-            InputMode::Search(_) => (
-                vec![
-                    Span::raw("Press "),
-                    Span::styled(
-                        "Esc",
-                        Style::default().fg(Color::Red).add_modifier(Modifier::BOLD),
-                    ),
-                    Span::raw(" to stop search, "),
-                    Span::styled(
-                        "Enter",
-                        Style::default().fg(Color::Red).add_modifier(Modifier::BOLD),
-                    ),
-                    Span::raw(" to goto selected node."),
-                ],
-                Style::default(),
-            ),
-            InputMode::Filter => (
-                vec![
-                    Span::raw("Press "),
-                    Span::styled(
-                        "Esc",
-                        Style::default().fg(Color::Red).add_modifier(Modifier::BOLD),
-                    ),
-                    Span::raw(" to stop filter, "),
-                    Span::styled(
-                        "Enter",
-                        Style::default().fg(Color::Red).add_modifier(Modifier::BOLD),
-                    ),
-                    Span::raw(" to apply filter."),
-                ],
-                Style::default(),
-            ),
-        },
-    };
-    let mut text = Text::from(Spans::from(msg));
-    text.patch_style(style);
+fn draw_help<B: Backend>(f: &mut Frame<B>, chunk: Rect) {
+    let text = vec![
+        Span::raw("Press "),
+        Span::styled("?", Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)),
+        Span::raw(" for help. "),
+        Span::raw("Press "),
+        Span::styled("q", Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)),
+        Span::raw(" to exit"),
+    ];
+    let mut text = Text::from(Spans::from(text));
+    text.patch_style(Style::default().add_modifier(Modifier::RAPID_BLINK));
 
     let help = Paragraph::new(text);
     f.render_widget(help, chunk);
