610ad87ffa862888e6a2563b2d388571ed897060
jaehyun1ee-furiosa <jaehyun.lee@furiosa.ai>
2023-02-17 13:31:39 +0900

    refactor: better help message

--- src/viewer/help.rs
+++ src/viewer/help.rs
@@ -9,26 +9,31 @@ impl Help {
         let header = header.iter().map(|s| s.to_string()).collect();
 
         let rows = vec![
-            vec!["All", "q", "",  "quit dot-viewer"],
-            vec!["", "esc", "",  "go back to Normal mode"],
+            vec!["Quit", "q", "", "quit dot-viewer"],
+            vec!["Help", "", ":help<CR>", "help"],
+
+            vec!["", "", "", ""],
+            vec!["All", "esc", "", "go back to Normal mode"],
+            vec!["Normal", "/", "", "go to fuzzy search mode"],
+            vec!["Normal", "r", "", "go to regex search mode"],
+            vec!["Normal", ":", "", "go to command mode"],
+
+            vec!["", "", "", ""],
             vec!["Normal", "c", "",  "close the current tab (view)"],
             vec!["", "h/l", "",  "move focus between current, prevs, nexts list"],
             vec!["", "j/k", "",  "traverse in focused list"],
+            vec!["", "n/N", "", "go to next/previous match"],
             vec!["", "tab/backtab", "",  "move between tabs"],
-            vec!["Search", "/", "", "go to fuzzy Search mode"],
-            vec!["", "r", "", "go to regex Search mode"],
-            vec!["", "tab", "", "autocomplete search keyword"], 
+            vec!["Search", "tab", "", "autocomplete search keyword"], 
             vec!["", "enter", "", "apply search"],
-            vec!["Command", ":", "", "go to Command mode"],
-            vec!["", "", "filter", "apply filter on current matches"],
+            vec!["Command", "", "filter", "apply filter on current matches"],
             vec!["", "", "neighbors [depth]", "export [depth] neighbors of the current node to dot"],
             vec!["", "", "export", "export the current tab (view) to dot"],
             vec!["", "", "xdot", "launch xdot on exports/current.dot"],
             vec!["", "", "subgraph", "go to subgraph Popup mode"],
             vec!["", "tab", "", "autocomplete command"],
             vec!["", "enter", "", "execute command"],
-            vec!["Subgraph Popup", "h/l", "", "open/close tree"],
-            vec!["", "j/k", "", "traverse tree"],
+            vec!["Subgraph Popup", "h/j/k/l", "", "traverse tree"],
             vec!["", "enter", "", "change root to the selected subgraph"],
         ];
         let rows = rows.iter().map(|row| row.iter().map(|s| s.to_string()).collect()).collect();
